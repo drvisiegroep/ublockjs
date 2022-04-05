413f17e869c0d8abdb816fc9b621c2070264b412
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-05 20:59:14 +0200

    Update ublockjs.js

--- ublockjs.js
+++ ublockjs.js
@@ -1,2 +1,5 @@
-document.getElementById("originalDiv").className = "banner_ad1";
-console.log("Hello, I'm running via scriptlet injection.");
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("originalDiv").className = "banner_ad1";
+    console.log("Hello, I'm running via scriptlet injection.");
+});
+
