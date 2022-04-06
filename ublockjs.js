f4160f45eb8b4e41553136a6464c52fa8b1d8c75
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-06 19:00:15 +0200

    Update ublockjs.js

--- ublockjs.js
+++ ublockjs.js
@@ -1,6 +1,6 @@
 /// ublockjs.js
 /// alias rm_ad.js
-console.log('ublock scriptlet');
+console.log('ublock scriptlet1');
 window.addEventListener('DOMContentLoaded', (event) => {
   document.getElementById("originalDiv").className = "bla";
   console.log('banner_ad')
