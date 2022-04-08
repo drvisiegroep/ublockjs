68795fb9931db4405a0015624eeb57f8dbe705a7
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-08 18:48:32 +0200

    Update ublockjs.js

--- ublockjs.js
+++ ublockjs.js
@@ -3,10 +3,11 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
   document.getElementById("originalDiv").className = "bla";
+  console.log('ublock scriptlet')
 });
 
 /// muted.js
 window.addEventListener('DOMContentLoaded', (event) => {
-  const mutebtn = document.querySelector('SmartxPlayer__unmuteVideoWrapperDesktop__1Dk76');
+  const mutebtn = document.querySelector('.SmartxPlayer__unmuteVideoWrapperDesktop__1Dk76');
   mutebtn.className = "bla";
 });
