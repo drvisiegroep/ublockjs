e9b2e93f2b19210498643c20b4d34c4958706261
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-08 20:38:46 +0200

    Update ublockjs.js

--- ublockjs.js
+++ ublockjs.js
@@ -1,9 +1,4 @@
-(function() {
-// >>>> start of private namespace
-'use strict';
-
   /// ublockjs.js
-  /// alias rm_ad.js
   window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("originalDiv").className = "bla";
     console.log('ublock scriptlet');
@@ -14,7 +9,3 @@
     document.querySelector('.SmartxPlayer__unmuteVideoWrapperDesktop__1Dk76').className = "bla";
     console.log('ublock scriptlet');
   });
-
-// These lines below are skipped by the resource parser.
-// <<<< end of private namespace
-})();
