5c19773a27cdf5870b32135a56cf9df3680bcee3
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-04-08 19:18:04 +0200

    Update ublockjs.js

--- ublockjs.js
+++ ublockjs.js
@@ -1,13 +1,21 @@
-/// ublockjs.js
-/// alias rm_ad.js
-
 (function() {
-  document.getElementById("originalDiv").className = "bla";
-  console.log('ublock scriptlet');
-});
+// >>>> start of private namespace
+'use strict';
 
-/// muted.js
-(function() {
-  document.querySelector('.SmartxPlayer__unmuteVideoWrapperDesktop__1Dk76').className = "bla";
-  console.log('ublock scriptlet');
-});
+  /// ublockjs.js
+  /// alias rm_ad.js
+
+  (function() {
+    document.getElementById("originalDiv").className = "bla";
+    console.log('ublock scriptlet');
+  });
+
+  /// muted.js
+  (function() {
+    document.querySelector('.SmartxPlayer__unmuteVideoWrapperDesktop__1Dk76').className = "bla";
+    console.log('ublock scriptlet');
+  });
+
+// These lines below are skipped by the resource parser.
+// <<<< end of private namespace
+})();
